a3595e708908bcd85005921b6da14308cc71433d
Nathan Petrangelo <nathan@petrangelo.org>
2021-04-19 03:09:33 -0400

    Print PSI element when it's a reference expression and says "Scene"

--- src/main/kotlin/com/github/npetrangelo/manimplugin/ManimMarkerContributor.kt
+++ src/main/kotlin/com/github/npetrangelo/manimplugin/ManimMarkerContributor.kt
@@ -2,24 +2,17 @@ package com.github.npetrangelo.manimplugin
 
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.psi.PsiElement
-import com.jetbrains.python.PyTokenTypes;
-import com.jetbrains.python.codeInsight.dataflow.scope.ScopeUtil;
-import com.jetbrains.python.psi.PyFile;
-import com.jetbrains.python.psi.PyIfStatement;
-import com.jetbrains.python.psi.PyUtil;
+import com.jetbrains.python.PythonFileType
+import com.jetbrains.python.psi.*
 
-class ManimMarkerContributor() : RunLineMarkerContributor() {
+object ManimMarkerContributor : RunLineMarkerContributor() {
     override fun getInfo(element: PsiElement): Info? {
-        if (isMainClauseOnTopLevel(element)) {
-
+        if (element.containingFile.fileType !is PythonFileType) {
+            return null
+        }
+        if (element is PyReferenceExpression && element.text == "Scene") {
+            println(element)
         }
         return null
-    }
-
-    private fun isMainClauseOnTopLevel(element: PsiElement): Boolean {
-        if (element.node.elementType == PyTokenTypes.IF_KEYWORD) {
-
-        }
-        return false
     }
 }

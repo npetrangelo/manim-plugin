7478a13deb1c6e92346d9515f45d58c4a701a34e
Nathan Petrangelo <nathan@petrangelo.org>
2021-04-19 03:27:14 -0400

    RunLineMarker shows up, but doesn't work

--- src/main/kotlin/com/github/npetrangelo/manimplugin/ManimMarkerContributor.kt
+++ src/main/kotlin/com/github/npetrangelo/manimplugin/ManimMarkerContributor.kt
@@ -1,9 +1,15 @@
 package com.github.npetrangelo.manimplugin
 
+import com.intellij.execution.lineMarker.ExecutorAction
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiElement
+import com.intellij.util.containers.ContainerUtil
 import com.jetbrains.python.PythonFileType
 import com.jetbrains.python.psi.*
+
 
 object ManimMarkerContributor : RunLineMarkerContributor() {
     override fun getInfo(element: PsiElement): Info? {
@@ -12,6 +18,16 @@ object ManimMarkerContributor : RunLineMarkerContributor() {
         }
         if (element is PyReferenceExpression && element.text == "Scene") {
             println(element)
+            val actions: Array<AnAction> = ExecutorAction.getActions()
+            val tooltipProvider = { psiElement: PsiElement ->
+                StringUtil.join(ContainerUtil.mapNotNull(actions) { action ->
+                    getText(
+                        action,
+                        psiElement
+                    )
+                }, "\n")
+            }
+            return Info(AllIcons.RunConfigurations.TestState.Run, tooltipProvider, actions)
         }
         return null
     }

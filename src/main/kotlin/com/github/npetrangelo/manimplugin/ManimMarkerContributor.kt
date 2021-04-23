ee04f07c6a23f13286416317b837d758705c698e
Nathan Petrangelo <nathan@petrangelo.org>
2021-04-23 17:03:08 -0400

    isManimScene() checks for Python file type

--- src/main/kotlin/com/github/npetrangelo/manimplugin/ManimMarkerContributor.kt
+++ src/main/kotlin/com/github/npetrangelo/manimplugin/ManimMarkerContributor.kt
@@ -4,15 +4,11 @@ import com.intellij.execution.lineMarker.ExecutorAction
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.icons.AllIcons
 import com.intellij.psi.PsiElement
-import com.jetbrains.python.PythonFileType
 import com.jetbrains.python.psi.PyClass
 
 
 object ManimMarkerContributor : RunLineMarkerContributor() {
     override fun getInfo(element: PsiElement): Info? {
-        if (element.containingFile.fileType !is PythonFileType) {
-            return null
-        }
         if (ManimUtil.isManimScene(element)) {
             return Info(AllIcons.RunConfigurations.TestState.Run,
                 {"[Manim] Render ${(element as PyClass).name}"},
--- src/main/kotlin/com/github/npetrangelo/manimplugin/ManimUtil.kt
+++ src/main/kotlin/com/github/npetrangelo/manimplugin/ManimUtil.kt
@@ -1,11 +1,15 @@
 package com.github.npetrangelo.manimplugin
 
 import com.intellij.psi.PsiElement
+import com.jetbrains.python.PythonFileType
 import com.jetbrains.python.psi.PyClass
 
 class ManimUtil {
     companion object {
         fun isManimScene(element: PsiElement): Boolean {
+            if (element.containingFile.fileType !is PythonFileType) {
+                return false
+            }
             if (element !is PyClass) return false
             element.getSuperClasses(null).forEach {
                 if (it.name == "Scene" &&

02244e6614203ea9d27d4c2b7dafa44b2d3e8e80
Nathan Petrangelo <nathan@petrangelo.org>
2021-04-23 17:01:15 -0400

    Removed imports

--- src/main/kotlin/com/github/npetrangelo/manimplugin/ManimMarkerContributor.kt
+++ src/main/kotlin/com/github/npetrangelo/manimplugin/ManimMarkerContributor.kt
@@ -3,10 +3,7 @@ package com.github.npetrangelo.manimplugin
 import com.intellij.execution.lineMarker.ExecutorAction
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.icons.AllIcons
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiElement
-import com.intellij.util.containers.ContainerUtil
 import com.jetbrains.python.PythonFileType
 import com.jetbrains.python.psi.PyClass
 
